84c10569ccbffc1b7da36907798ff3e87f41519e
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-18 10:25:56 +0100

    lecture 3 example

---
+++ lect_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+		int* int_array = malloc(argv[1])
+		
+		for (int i = 0; i < argv[1]; i++){
+			int_array[i] = i;
+			printf("%p %d\n", &int_array[i], *int_array[i])
+			
