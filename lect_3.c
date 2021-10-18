1cc654b199ee22974551259e562e8c8578745047
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-18 12:32:13 +0100

    Update lect_3.c

--- lect_3.c
+++ lect_3.c
@@ -4,13 +4,14 @@
 int main(int argc, char *argv[]){
 
 	int *int_array = malloc(atoi(argv[1])* sizeof(int));
+	int *ptr = &int_array;
 	
 		int value = 0;
 		while (value < atoi(argv[1])){
-			*int_array = value;
-			printf("%p %d\n", &int_array[i], int_array[i]);
-			value ++
-			int_array++
+			*ptr = value;
+			printf("%p %d\n", &int_array[value], int_array[value]);
+			value ++;
+			ptr++;
 		}
 }		
 			
