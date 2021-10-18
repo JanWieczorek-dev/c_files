cee6c194358281b1b517500d4380a21e66415a88
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-18 10:49:09 +0100

    Update lect_3.c

--- lect_3.c
+++ lect_3.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]){
 
-		int* int_array = malloc(argv[1] * sizeof(int);
+		int* int_array = malloc(*argv[1] * sizeof(int);
 		
 		for (int i = 0; i < *argv[1]; i++){
 			int_array[i] = i;
