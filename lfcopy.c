b446dd0f7426f4031c9045e4acab9c313648331e
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-21 22:41:31 +0100

    Update lfcopy.c

--- lfcopy.c
+++ lfcopy.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <string.h> 
+#include <string.h>
 
+#define MAXLINE 1024
 
 int readline(char line[], int max);
 int writeline(const char line[]);
 
+int main()
+{
+	char buf[MAXLINE];
 
-int readline(char line[], int max){
+	while (readline(buf, MAXLINE))
+		writeline(buf);
+	return 0;
+}
 
+/* readline: read a line from standard input, return its length or 0 */
+int readline(char line[], int max)
+{
 	if (fgets(line, max, stdin) == NULL)
 		return 0;
 	else
 		return strlen(line);
 }
 
-
-int writeline(const char line[]){
-
+/* writeline: write line to standard output, return number of chars written */
+int writeline(const char line[])
+{
 	fputs(line, stdout);
-	
 	return strlen(line);
-}
-
-
-int main() {
-
-	char input_line[INT_MAX]; 
-
-	while (readline( input_line, INT_MAX)){
-	
-		writeline(input_line);
-	
-	return 0;
-	}
-	
-	
 }
