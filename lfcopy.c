1fa10fdac93dd26fb150d44003e2a5383f54c785
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-21 22:38:15 +0100

    Update lfcopy.c

--- lfcopy.c
+++ lfcopy.c
@@ -4,29 +4,37 @@
 #include <string.h> 
 
 
+int readline(char line[], int max);
+int writeline(const char line[]);
+
+
 int readline(char line[], int max){
 
-		if (fgets(line, max, stdin) == NULL)
-			return 0;
-		else
-			return strlen(line);
-	}
-
-
-	int writeline(const char line[]){
-
-		fputs(line, stdout);
-	
+	if (fgets(line, max, stdin) == NULL)
+		return 0;
+	else
 		return strlen(line);
-	}
+}
+
+
+int writeline(const char line[]){
+
+	fputs(line, stdout);
+	
+	return strlen(line);
+}
 
 
 int main() {
 
 	char input_line[INT_MAX]; 
 
-	while (readline( input_line, INT_MAX) != 0){
+	while (readline( input_line, INT_MAX)){
 	
 		writeline(input_line);
+	
+	return 0
 	}
+	
+	
 }
