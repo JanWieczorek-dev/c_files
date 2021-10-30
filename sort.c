60d5e0ba31e04ac2f6eb441f8f0bbed23c4ea107
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-10-30 18:09:26 +0100

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>  
+#include<string.h>  
+
+
+struct name {
+const char * first_name;
+const char * last_name;
+};
+typedef struct name name
+
+
+ int compare_first (name name1, name name1){
+	 return int strcmp (name1->first_name, name2->first_name)
+ }
+ 
+ 
+ int compare_last (name name1, name name1){
+	 return int strcmp (name1->last_name, name2->last_name)
+ }
+
+int main() {
+	
+name names[4] = {
+ {"Grace", "Hopper"},
+ {"Dennis", "Ritchie"},
+ {"Ken", "Thompson"},
+ {"Bjarne", "Stroustrup"}
+};
+
+qsort(names, 4, sizeof(name), compare_last) 	 
+
+for (int i = 0; i < 4; i++){
+	printf("%s\n", names[i]->last_name)
+}
+
+printf("%s", "by first name:")
+
+qsort(names, 4, sizeof(name), compare_last) 	
+
+for (int i = 0; i < 4; i++){
+	printf("%s\n", names[i]->first_name)
+}
