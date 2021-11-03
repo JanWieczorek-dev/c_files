6b39fdf0f91e30295f0b938e063f9a7b9d41afc3
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-11-03 21:08:36 +0000

    Update test2.c

--- test2.c
+++ test2.c
@@ -9,13 +9,13 @@ int main(){
     char* str1 = "ab";
     char* str2 = "ABC";
     int i =0;
-    char* new_str[3];
+    char new_str[3];
 
     printf("%d \n", strncmp(str1, str2, 3));
 
     while (i < 3)
     {
-        *new_str[i] = tolower(str2[i]);
+        new_str[i] = tolower(str2[i]);
         i++;
 
     }
