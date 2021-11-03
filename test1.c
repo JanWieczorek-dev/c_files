bcc6d598cdab5912d3eb48665314ba536df7460b
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-11-03 18:06:14 +0000

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,23 @@
+#include "tldlist.h"
+#include <stdio.h>
+#include <string.h>
+#include "date.c"
+
+
+int main(){
+
+    char *date;
+    char * string = "05/11/2018 www.intel.com";
+    char *tld;
+    Date processed_date;
+
+    sscanf(string, "%s %s", date, tld);
+
+    processed_date = *date_create(date);
+
+    printf("%s %s", tld, date );
+    
+
+
+
+}
